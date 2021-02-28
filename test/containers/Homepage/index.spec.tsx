80c67e191d1e5377cf96168c56e446e49c1b7953
Joshua Sherman <joshua.v.sherman@gmail.com>
2021-02-28 15:14:22 -0500

    finishes unit tests

--- test/containers/Homepage/index.spec.tsx
+++ test/containers/Homepage/index.spec.tsx
@@ -3,8 +3,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Homepage } from '../../../src/containers/Homepage';
 
+const testBlogs = [
+  { _id: '1', title: 'title1', body: 'body1' }, { _id: '2', title: 'title2', body: 'body2' }, { _id: '3', title: 'title3', body: 'body3' },
+  { _id: 'badBlog' },
+];
 const targetRef:any = {};
-const wrapper = shallow<Homepage>(<Homepage targetRef={targetRef} width={1000} height={800} blogs={[]} />);
+const wrapper = shallow<Homepage>(<Homepage targetRef={targetRef} width={1000} height={800} blogs={testBlogs} />);
 
 describe('Home', () => {
   it('renders snapshot correctly', () => { expect(wrapper).toMatchSnapshot(); });
