eadab107644ba63e9cb282dc0c929a0e9d882e64
Joshua Sherman <joshua.v.sherman@gmail.com>
2021-03-16 07:59:19 -0400

    finishes blog editor unit tests

--- src/components/BlogEditor/index.tsx
+++ src/components/BlogEditor/index.tsx
@@ -40,11 +40,11 @@ const BlogEditor = ({
             <br />
             {utils.editor(body, comp.handleEditorChange)}
           </label>
-          <div style={{ marginLeft: '10px', marginTop: '10px' }}>
+          <div style={{ marginLeft: '10px', marginTop: '10px' }} id="update-blog-button-div">
             <button
               style={{ marginRight: '10px' }}
               type="button"
-              id="update-youthContent"
+              id="update-blog-button"
               disabled={false}
               onClick={() => comp.putAPI()}
             >
@@ -53,7 +53,7 @@ const BlogEditor = ({
             <button
               style={{ marginRight: '10px' }}
               type="button"
-              id="update-youthContent"
+              id="cancel-blog-update-button"
               disabled={false}
               onClick={() => comp.setState({ editBlog: { _id: '', title: '', body: '' } })}
             >
---
+++ test/components/BlogEditor/index.spec.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react';
+import { shallow, ShallowWrapper } from 'enzyme';
+import BlogEditor from '../../../src/components/BlogEditor';
+import { IBlog } from '../../../src/containers/Homepage';
+
+describe('Dashboard Container', () => {
+  let wrapper:ShallowWrapper<any>, compStub: any, editBlog:IBlog;
+  beforeEach(() => {
+    editBlog = { title: '', _id: '123', body: '' };
+    compStub = {
+      setState: jest.fn(),
+      putAPI: jest.fn(),
+    };
+    wrapper = shallow<typeof BlogEditor>(<BlogEditor
+      comp={compStub}
+      editBlog={editBlog}
+    />);
+  });
+  it('renders snapshot correctly', () => { expect(wrapper).toMatchSnapshot(); });
+  it('handles onChange for blog title', () => {
+    const myForm = wrapper.find('#update-blog123');
+    myForm.find('#blogtitle').simulate('change', { target: { value: 'howdy' } });
+    expect(compStub.setState).toHaveBeenCalledWith({ editBlog: { title: 'howdy', _id: '123', body: '' } });
+  });
+  it('handles click for update-blog-button', () => {
+    const myForm = wrapper.find('#update-blog123');
+    const buttonDiv = myForm.find('#update-blog-button-div');
+    buttonDiv.find('#update-blog-button').simulate('click');
+    expect(compStub.putAPI).toHaveBeenCalled();
+  });
+  it('handles click for cancel-blog-update-button', () => {
+    const myForm = wrapper.find('#update-blog123');
+    const buttonDiv = myForm.find('#update-blog-button-div');
+    buttonDiv.find('#cancel-blog-update-button').simulate('click');
+    expect(compStub.setState).toHaveBeenCalled();
+  });
+});
