eac9a05cfc63c7077ec8b5ff6f7825726e5e646a
Rhayven <codingforllamas@gmail.com>
2021-04-09 22:33:51 -0400

    More fixes

--- src/App/AppTemplate.tsx
+++ src/App/AppTemplate.tsx
@@ -136,10 +136,9 @@ export class AppTemplate extends React.Component<AppMainProps, AppMainState> {
   }
 
   // eslint-disable-next-line class-methods-use-this
-  sidebar(): JSX.Element {
-    const { menuOpen } = this.state; const style = `${menuOpen ? 'open' : 'close'}`;
+  sidebar(className: string): JSX.Element {
     return (
-      <div className={`${style} sidebar__functions`}>
+      <div className={`${className} sidebar__functions`}>
         {// TODO remove process.env check when feature is working
         /* istanbul ignore next */process.env.NODE_ENV !== 'production'
         ? this.makeForm('Search') : null
@@ -165,33 +164,39 @@ export class AppTemplate extends React.Component<AppMainProps, AppMainState> {
     );
   }
 
+  drawerHeader(className: string): JSX.Element {
+    return (
+      <header className="header">
+        <div className="header__logo">
+          <img
+            className="header__logo--picture"
+            src="https://dl.dropbox.com/s/befh330hypey44l/fish%20with%20color.png?dl=0"
+            alt="The Jesus Fish with a multi-coloured background"
+          />
+          <span
+            className={`${className} header__mobile-menu`}
+            onClick={this.toggleMobileMenu}
+            onKeyPress={this.handleKeyMenu}
+            tabIndex={0}
+            role="button"
+          >
+            <span className="header__mobile-menu--icon" />
+          </span>
+          <span className="header__title">
+            <h2>Change In Christ</h2>
+          </span>
+        </div>
+      </header>
+    );
+  }
+
   drawerContainer(): JSX.Element {
     const { menuOpen } = this.state; const style = `${menuOpen ? 'open' : 'close'}`;
     return (
       <div className="sidebar">
         <div className="sidebar__content">
-          <header className="header">
-            <div className="header__logo">
-              <img
-                className="header__logo--picture"
-                src="https://dl.dropbox.com/s/befh330hypey44l/fish%20with%20color.png?dl=0"
-                alt="The Jesus Fish with a multi-coloured background"
-              />
-              <span
-                className={`${style} header__mobile-menu`}
-                onClick={this.toggleMobileMenu}
-                onKeyPress={this.handleKeyMenu}
-                tabIndex={0}
-                role="button"
-              >
-                <span className="header__mobile-menu--icon" />
-              </span>
-              <span className="header__title">
-                <h2>Change In Christ</h2>
-              </span>
-            </div>
-          </header>
-          {this.sidebar()}
+          {this.drawerHeader(style)}
+          {this.sidebar(style)}
           {this.navLinks(style)}
           {this.headerLogo2()}
         </div>
