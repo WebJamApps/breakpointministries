73c6df3ffe751d1801f3d4d03ae47b612ace8545
Logan Richards <logrdog15@gmail.com>
2021-04-25 02:15:12 -0400

    stumped

--- src/App/authUtils.ts
+++ src/App/authUtils.ts
@@ -19,7 +19,10 @@ async function setUser(view: AppTemplate): Promise<string> {
     decoded = jwt.verify(auth.token, process.env.HashString || /* istanbul ignore next */'');
   } catch (e) { return `${e.message}`; }
   // check if user has the crediential, if not log them out and do not do line 22. 
-  /*if(!auth.isAuthenticated && !auth.user.userType && JSON.parse(process.env.userRoles || '').roles.indexOf(decoded.auth.user.userType) === -1) {
+  // userType is null in props? yet what am i missing/overlooking? It gets authenticated in auth actions and then dispatched in an action if authenticated? 
+  // would I need to do a post request and then check if the email has the user typr from DB with a get?
+  // const userRoles: string[] = commonUtils.getUserRoles();
+  /*if(auth.user.userType && userRoles.indexOf(auth.user.userType) === -1) {
     responseGoogleLogout(dispatch);
     window.location.reload();
   }*/
