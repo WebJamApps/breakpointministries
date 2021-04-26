70d8024501ad3d55bdb0a7954410729add4dcf76
Joshua Sherman <joshua.v.sherman@gmail.com>
2021-04-26 07:17:57 -0400

    blogs are sorted now

--- src/redux/mapStoreToProps.ts
+++ src/redux/mapStoreToProps.ts
@@ -12,6 +12,10 @@ export interface Store {
   blogs: { blogs: any[] };
 }
 
+export interface Iblog {
+  _id:string, title:string, body:string, created_at:string, updated_at:string
+}
+
 const mapStoreToProps = (store: Store): Record<string, unknown> => ({
   auth: store.auth,
   blogs: store.blogs.blogs,
--- src/redux/reducers/blogReducer.ts
+++ src/redux/reducers/blogReducer.ts
@@ -1,13 +1,27 @@
+import type { Iblog } from '../mapStoreToProps';
+
 const initialState = {
   blogs: [],
 };
 
-const reducer = (state = initialState, action: { type: string; data: any[]; }): Record<string, unknown> => {
+const sortBlogs = (blogs:Iblog[]) => {
+  const sortedBlogs = blogs.sort((a, b) => {
+    const aTime = new Date(a.created_at).getTime();
+    const bTime = new Date(b.created_at).getTime();
+    if (aTime > bTime) return -1;
+    if (aTime < bTime) return 1;
+    return 0;
+  });
+  return sortedBlogs;
+};
+
+const reducer = (state = initialState, action: { type: string; data: Iblog[]; }): Record<string, unknown> => {
   switch (action.type) {
     case 'GOT_BLOGS':
       return {
         ...state,
-        blogs: action.data,
+        // blogs: action.data,
+        blogs: sortBlogs(action.data),
       };
     default:
       return state;
